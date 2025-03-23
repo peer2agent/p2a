d2da025cc01fb5a171c2abeb8162d8c31117b0c6
Txrmarcos <marcos.teixeira@sou.inteli.edu.br>
2025-03-23 15:45:52 -0300

    add - route teste

--- src/web/tracking-wallet/routes-v1.ts
+++ src/web/tracking-wallet/routes-v1.ts
@@ -6,11 +6,7 @@ import { TraderBotUseCase } from "../../usecase/impl/TraderBotUseCase";
 import { RealiseSwap } from "../../usecase/impl/RealiseSwapUseCase";
 import { TrackingInfoInputDTO } from "../../input/dto/TrackingInfoInputDTO";
 import { TransactionProcessorUseCase } from "../../usecase/impl/TransactionProcessorUseCase";
-import { WalletTrackerImpl } from "../../wallet-tracker-service/impl/WalletTrackerImpl";
-import { Webhook } from "helius-sdk";
-import { JupiterClientSwap } from "../../trade-token-service/client/JupiterClientSwap";
-import { Connection, Keypair, PublicKey } from "@solana/web3.js";
-import bs58 from "bs58";
+
 dotenv.config();
 
 const app = express();
@@ -56,55 +52,6 @@ app.post("/realise-trade", async (req, res) => {
   await realiseSwap.usecase(tracking);
 
   res.send("ok");
-})
-
-app.post("/test", async (req, res) => {
-  const url = "https://quote-api.jup.ag/v6/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=D5ZA9HSXrdoRFxNnUyki6NAd3sUrpZF72Hmda5M4xyBt&amount=100&slippageBps=1"
-  const response = await fetch(url)
-  const data = await response.json()
-  console.log("olha data",data)
-  var x = {
-    inAmount: data.inAmount,
-    otherAmountThreshold: data.otherAmountThreshold,
-    quoteResponse: data
-  }
-
-  const quoteResponse=x.quoteResponse
-    
-  const swapResponse = await (
-      await fetch('https://quote-api.jup.ag/v6/swap', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          // quoteResponse from /quote api
-          quoteResponse,
-          // user public key to be used for the swap
-          userPublicKey: "BrSe6VQsP2noN7RQ215aNYt8ZN33QyHQkdpBbGott9ro",
-          // auto wrap and unwrap SOL. default is true
-          wrapAndUnwrapSol: true,
-          // Optional, use if you want to charge a fee.  feeBps must have been passed in /quote API.
-          feeAccount: "BrSe6VQsP2noN7RQ215aNYt8ZN33QyHQkdpBbGott9ro",
-
-          prioritizationFeeLamports: 20000
-        })
-      })
-    ).json();
-
-    const { swapTransaction, lastValidBlockHeight } = swapResponse;
-
-    const keypairBase58 = process.env.SECRET_KEY!!;
-
-    const keypairBytes = bs58.decode(keypairBase58);
-
-    
-
-    var jupiter = new JupiterClientSwap(new Connection("https://api.mainnet-beta.solana.com"), false)
-
-    var txid = await jupiter.sendTransaction(swapTransaction, Keypair.fromSecretKey(keypairBytes), lastValidBlockHeight)
-
-    res.send(txid)
 })
 
 
